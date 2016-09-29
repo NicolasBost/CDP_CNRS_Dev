b0dcb29eb94b7c4e68b6d1e1dc599d1a2ddc818c
kbaptista <kevbap@gmail.com>
2016-09-30 00:58:49 +0200

    template v1 - lien de index vers ajouter atelier fait

---
+++ src/web/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta name="google-site-verification" content="aKipA0ol1qm2Ddbn4x8joCOC7xuGh_Fzdm3ho8B1iyg" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Accueil - Circuit Scientifique - CNRS</title>
+        <link href="http://fonts.googleapis.com/css?family=Raleway:400,200,500,600,700,800,300" rel="stylesheet" />
+        <link href="CSS/style.css" rel="stylesheet" type="text/css" media="all" />
+        <!--[if IE 6]>
+        <link href="default_ie6.css" rel="stylesheet" type="text/css" />
+        <![endif]-->
+    </head>
+    <body>
+        <div id="banner" class="under">
+            <h1 id="banner-title">Circuit Scientifique Bordelais</h1>
+        </div>
+        <div id="menu" class="under">
+            <button class="button">Les Ateliers</button>
+            <button class="button">Créez votre Atelier</button>
+        </div>
+        
+        <div id="content" class="under">
+            <article>
+                <h1>La Fête de la Science vous invite dans les Laboratoires Bordelais</h1>
+                <p>Dans le cadre de la Fête de la Science, le <a href="www.cnrs.fr" target="_blank">CNRS</a> vous invite à vivre le Circuit Scientifique Bordelais ! Vous pourrez ainsi participer à une grande diversité d'activités scientifiques, proposées et animées par des chercheurs professionnels.
+                </p>
+            </article>
+        </div>
+
+        <div id="footer">
+            <h3 class="under">Les Participants</h3>
+            <ul>
+                <li>
+                    <a href="www.cnrs.fr" target="_blank"><img src="./media/CNRSfr.jpg"></a>
+                </li>
+            </ul>
+        </div>
+
+    </body>
+</html>
