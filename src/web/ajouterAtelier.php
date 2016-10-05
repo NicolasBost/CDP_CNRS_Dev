dbf6ffa91da9afc6800f3b46f334123598d9053a
Nicolas <nicolas.bost@etu.u-bordeaux.fr>
2016-10-06 01:27:11 +0200

    Modif AjouterAtelier

--- src/web/ajouterAtelier.php
+++ src/web/ajouterAtelier.php
@@ -1,44 +1,64 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Ajouter Atelier</title>
+</head>
+<body>
+<center>
+<input type="text" name="title" size="20" value="Titre de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="theme" size="20" value="Thème de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="type" size="20" value="Type de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="duration" size="20" value="Durée de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="capacity" size="20" value="Capacité de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="abstract" size="20" value="Résumé de l'atelier" maxlength="35"/>
+<br>
+<input type="text" name="target" size="20" value="Cible de l'atelier" maxlength="35"/>
+<br>
+<input type="submit" value="Créer l'atelier" name="envoyer"/>
+</center>
+</form>
+</body>
+</html>
+<?php
+	$con = mysqli("localhost","root","root","GestionDeProjet");
+	if ($con->connect_error){
+		die("Pas de connection" . $con->connect_error);
+	}
+	echo "Connection effectuée"
 
-// On récupère les champs
+	$title = $_POST['title'];
+	$theme = $_POST['theme'];
+	$type = $_POST['type'];
+	$duration = $_POST['duration'];
+	$capacity = $_POST['capacity'];
+	$abstract = $_POST['abstract'];
+	$target = $_POST['target'];
 
-if(isset($_POST['title']))	$title=$_POST['title'];
-else	$title="";
+	$title = mysql_real_escape_string($title);
+	$theme = mysql_real_escape_string($theme);
+	$type = mysql_real_escape_string($type);
+	$duration = mysql_real_escape_string($duration);
+	$capacity = mysql_real_escape_string($capacity);
+	$abstract = mysql_real_escape_string($abstract);
+	$target = mysql_real_escape_string($target);
 
-if(isset($_POST['theme']))	$theme=$_POST['theme'];
-else	$theme="";
+	$query = "
+	INSERT INTO Atelier(title,theme,type,duration,capacity,abstract,target)
+	VALUES('$title','$theme','$type','$duration','$capacity','$abstract','$target');";
 
-if(isset($_POST['type']))	$type=$_POST['type'];
-else	$type="";
+	if ($con->query($query) === TRUE){
+    		echo "New record created successfully";
+	}
+	else{
+    		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 
-if(isset($_POST['duration']))	$duration=$_POST['duration'];
-else	$duration="";
+	$con->close();
 
-if(isset($_POST['capacity']))	$capacity=$_POST['capacity'];
-else	$capacity="";
-
-if(isset($_POST['abstract']))	$abstract=$_POST['abstract'];
-else	$abstract="";
-
-if(isset($_POST['target']))	$target=$_POST['target'];
-else	$target="";
-
-// On vérifie si les champs sont vides 
-if(empty($title) OR empty($theme) OR empty($type) OR empty($duration) OR empty($capacity) OR empty($abstract) OR empty($target)) 
-    { 
-    echo '<font color="red">Attention, champ(s) vide(nt) !</font>'; 
-    } 
-// Aucun champ n'est vide, on peut enregistrer dans la table 
-else      
-    { 
-    $db = mysql_connect('localhost', 'login', 'password');  // connexion à la base 
-    mysql_select_db('GestionDeProjet',$db);                  // sélection de la base 
-     $sql = "INSERT INTO Atelier(title, theme, type, duration, capacity, abstract, target) VALUES('$title','$theme','$type','$duration','$capacity','$abstract', '$target')"; 
-     mysql_query($sql) or die('Erreur SQL !'.$sql.'<br>'.mysql_error());  
-         
-     // on affiche le résultat pour le visiteur 
-     echo 'Vos infos on été ajoutées.'; 
-        } 
-    mysql_close();  // on ferme la connexion 
-    }  
-?> 
+?>
