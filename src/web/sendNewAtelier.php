54b6bdf5dcbbb795445a4328f775dafb2a571bef
Nicolas <nicolas.bost@etu.u-bordeaux.fr>
2016-10-06 12:00:07 +0200

    final ajout atelier

--- src/web/sendNewAtelier.php
+++ src/web/sendNewAtelier.php
@@ -1,45 +1,38 @@
 <html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>send data</title>
+</head>
 <body>
-
-titre <?php echo $_POST["title"]; ?><br>
-theme <?php echo $_POST["theme"]; ?>
-
-</body>
-</html>
 <?php
-	$con = mysqli_connect("localhost","root","root","GestionDeProjet");
+	$title=$_POST['title'];
+	$theme=$_POST["theme"];
+	$type=$_POST["type"];
+	$duration=$_POST["duration"];
+	$capacity=$_POST["capacity"];
+	$abstract=$_POST["abstract"];
+	$target=$_POST["target"];
+	$servername="localhost";
+	$username="root";
+	$password="root";
+	$base="GestionDeProjet";
+	$con = mysqli_connect($servername,$username,$password,$base);
 	if (!$con){
-		die("Pas de connexion" . $con->connect_error);
+		die("Pas de connexion" . mysqli_connect_error());
 	}
-	echo "Connexion effectuée"
-
-	$title = $_POST['title'];
-	$theme = $_POST['theme'];
-	$type = $_POST['type'];
-	$duration = $_POST['duration'];
-	$capacity = $_POST['capacity'];
-	$abstract = $_POST['abstract'];
-	$target = $_POST['target'];
-
-	$title = mysql_real_escape_string($title);
-	$theme = mysql_real_escape_string($theme);
-	$type = mysql_real_escape_string($type);
-	$duration = mysql_real_escape_string($duration);
-	$capacity = mysql_real_escape_string($capacity);
-	$abstract = mysql_real_escape_string($abstract);
-	$target = mysql_real_escape_string($target);
-
-	$query = "
-	INSERT INTO Atelier(title,theme,type,duration,capacity,abstract,target)
-	VALUES('$title','$theme','$type','$duration','$capacity','$abstract','$target');";
-
+	echo "Connexion effectuée";
+	
+	$query="INSERT INTO Atelier(title,theme,type,duration,capacity,abstract,target)
+VALUES('$title','$theme','$type',$duration,$capacity,'$abstract','$target');";
 	if ($con->query($query) === TRUE){
     		echo "New record created successfully";
 	}
 	else{
-    		echo "Error: " . $sql . "<br>" . $conn->error;
+    		echo "Error: " . $sql . "<br>" . $con->error;
 	}
 
-	$con->close();
 
+	
 ?>
+</body>
+</html>
